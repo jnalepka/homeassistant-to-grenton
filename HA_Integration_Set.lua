fa695296c567b2590df1deb04b929e5de00aaa7a
Jan Nalepka <70645322+jnalepka@users.noreply.github.com>
2024-10-07 10:08:55 +0200

    Create HA_Integration_Set.lua

---
+++ HA_Integration_Set.lua
@@ -0,0 +1,37 @@
+-- ╔═══════════════════════════════════════════════════════════════════════╗
+-- ║                        Author: Jan Nalepka                            ║
+-- ║                                                                       ║
+-- ║ Script: HA_Integration_Set                                            ║
+-- ║ Description: Send a service command for an entity in Home Assistant.  ║
+-- ║                                                                       ║
+-- ║ License: MIT License                                                  ║
+-- ║ Github: https://github.com/jnalepka                                   ║
+-- ║                                                                       ║
+-- ║ Version: 1.0.0                                                        ║
+-- ║                                                                       ║
+-- ║ Requirements:                                                         ║
+-- ║    Gate Http:                                                         ║
+-- ║          1.  Gate Http NAME: "GATE_HTTP" <or change it in this script>║
+-- ║                                                                       ║
+-- ║    Script parameters:                                                 ║
+-- ║          1.  ha_entity, default: "light.my_lamp", string              ║
+-- ║          2.  ha_method, default: "toggle", string                     ║
+-- ║                                                                       ║
+-- ║    Http_Request virtual object:                                       ║
+-- ║          Name: HA_Request_Set                                         ║
+-- ║          Host: http://192.168.0.114:8123  (example)                   ║
+-- ║          Path: /api/state (any value)                                 ║
+-- ║          Method: "POST"                                               ║
+-- ║          RequestType: JSON                                            ║
+-- ║          ResponseType: JSON                                           ║
+-- ║          RequestHeaders: Authorization: Bearer <your HA token>        ║
+-- ║                                                                       ║
+-- ╚═══════════════════════════════════════════════════════════════════════╝
+
+local ha_service, ha_entity_name = string.match(ha_entity, "([^%.]+)%.([^%.]+)")
+local path = "/api/services/"..ha_service.."/"..ha_method
+local reqJson = { entity_id = ha_entity }
+
+GATE_HTTP->HA_Request_Set->SetPath(path)
+GATE_HTTP->HA_Request_Set->SetRequestBody(reqJson)
+GATE_HTTP->HA_Request_Set->SendRequest()
