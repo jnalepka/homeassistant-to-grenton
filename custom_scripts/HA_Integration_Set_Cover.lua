be3f70c3f59c734aefa31d59fd7b50c14717e9d6
Jan Nalepka <70645322+jnalepka@users.noreply.github.com>
2025-01-11 00:14:33 +0100

    Update HA_Integration_Set_Cover.lua

--- custom_scripts/HA_Integration_Set_Cover.lua
+++ custom_scripts/HA_Integration_Set_Cover.lua
@@ -1,46 +1,50 @@
--- ╔═══════════════════════════════════════════════════════════════════════════════╗
--- ║                        Author: Jan Nalepka                                    ║
--- ║                                                                               ║
--- ║ Script: HA_Integration_Set_Cover                                              ║
--- ║ Description: Send a service command for an entity in Home Assistant.          ║
--- ║                                                                               ║
--- ║ License: MIT License                                                          ║
--- ║ Github: https://github.com/jnalepka/homeassistant-to-grenton                  ║
--- ║                                                                               ║
--- ║ Version: 1.0.0                                                                ║
--- ║                                                                               ║
--- ║ Requirements:                                                                 ║
--- ║    Gate Http:                                                                 ║
--- ║          1.  Gate Http NAME: "GATE_HTTP" <or change it in this script>        ║
--- ║                                                                               ║
--- ║    Script parameters:                                                         ║
--- ║          1.  ha_entity, default: "select.my_options", string                  ║
--- ║          2.  ha_method, default: "select_next", string                        ║
--- ║          3.  attr_position, default: -1, number [0-100]                       ║
--- ║          4.  attr_tilt_position, default: -1, number [0-100]                  ║
--- ║                                                                               ║
--- ║    Http_Request virtual object:                                               ║
--- ║          Name: HA_Request_Set                                                 ║
--- ║          Host: http://192.168.0.114:8123  (example)                           ║
--- ║          Path: /api/state (any value)                                         ║
--- ║          Method: "POST"                                                       ║
--- ║          RequestType: JSON                                                    ║
--- ║          ResponseType: JSON                                                   ║
--- ║          RequestHeaders: Authorization: Bearer <your HA token>                ║
--- ║                                                                               ║
--- ║    Available methods:                                                         ║
--- ║          -  open_cover                                                        ║
--- ║          -  close_cover                                                       ║
--- ║          -  toggle                                                            ║
--- ║          -  set_cover_position(attr_position)   range=[0-100] %               ║
--- ║          -  stop_cover                                                        ║
--- ║          -  open_cover_tilt                                                   ║
--- ║          -  close_cover_tilt                                                  ║
--- ║          -  toggle_cover_tilt                                                 ║
--- ║          -  set_cover_tilt_position(attr_tilt_position)   range=[0-100] %     ║
--- ║          -  stop_cover_tilt                                                   ║
--- ║                                                                               ║
--- ╚═══════════════════════════════════════════════════════════════════════════════╝
+-- ╔══════════════════════════════════════════════════════════════════════════════════════════════════════╗
+-- ║                        Author: Jan Nalepka                                                           ║
+-- ║                                                                                                      ║
+-- ║ Script: HA_Integration_Set_Cover                                                                     ║
+-- ║ Description: Send a service command for an entity in Home Assistant.                                 ║
+-- ║                                                                                                      ║
+-- ║ License: MIT License                                                                                 ║
+-- ║ Github: https://github.com/jnalepka/homeassistant-to-grenton                                         ║
+-- ║                                                                                                      ║
+-- ║ Version: 1.0.0                                                                                       ║
+-- ║                                                                                                      ║
+-- ║ Requirements:                                                                                        ║
+-- ║    Gate Http:                                                                                        ║
+-- ║          1.  Gate Http NAME: "GATE_HTTP" <or change it in this script>                               ║
+-- ║                                                                                                      ║
+-- ║    Script parameters:                                                                                ║
+-- ║          1.  ha_entity, default: "cover.my_blind", string                                            ║
+-- ║          2.  ha_method, default: "toggle", string                                                    ║
+-- ║          3.  attr_position, default: -1, number [0-100]                                              ║
+-- ║          4.  attr_tilt_position, default: -1, number [0-100]                                         ║
+-- ║                                                                                                      ║
+-- ║    Http_Request virtual object:                                                                      ║
+-- ║          Name: HA_Request_Set                                                                        ║
+-- ║          Host: http://192.168.0.114:8123  (example)                                                  ║
+-- ║          Path: /api/state (any value)                                                                ║
+-- ║          Method: "POST"                                                                              ║
+-- ║          RequestType: JSON                                                                           ║
+-- ║          ResponseType: JSON                                                                          ║
+-- ║          RequestHeaders: Authorization: Bearer <your HA token>                                       ║
+-- ║                                                                                                      ║
+-- ║    Available methods:                                                                                ║
+-- ║          -  open_cover                                                                               ║
+-- ║          -  close_cover                                                                              ║
+-- ║          -  toggle                                                                                   ║
+-- ║          -  set_cover_position(attr_position)   range=[0-100] %                                      ║
+-- ║          -  stop_cover                                                                               ║
+-- ║          -  open_cover_tilt                                                                          ║
+-- ║          -  close_cover_tilt                                                                         ║
+-- ║          -  toggle_cover_tilt                                                                        ║
+-- ║          -  set_cover_tilt_position(attr_tilt_position)   range=[0-100] %                            ║
+-- ║          -  stop_cover_tilt                                                                          ║
+-- ║                                                                                                      ║
+-- ║    Examples:                                                                                         ║
+-- ║          - entity="cover.blind1", method="set_cover_position", position=100                          ║
+-- ║          - entity="cover.blind1", method="set_cover_tilt_position", tilt_position=100                ║
+-- ║                                                                                                      ║
+-- ╚══════════════════════════════════════════════════════════════════════════════════════════════════════╝
 
 local ha_service, ha_entity_name = string.match(ha_entity, "([^%.]+)%.([^%.]+)")
 local path = "/api/services/"..ha_service.."/"..ha_method
